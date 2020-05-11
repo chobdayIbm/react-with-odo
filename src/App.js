b5397b5d7546c41ccad2b8913a252ece465d5975
Colin Hobday <chobday@sg.ibm.com>
2020-05-11 16:15:17 +0800

    list of IPs - 1st draft

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const ipAddresses = ['5.39.70.218', '45.9.148.25']
+  const listItems = ipAddresses.map((addr) => 
+    <li>{addr}</li>
+  )
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Visitors</h2>
+        <ul>{listItems}</ul>
       </header>
     </div>
   );
